8f3c21d93f95c9887350a327d0a2806f58d3983e
admin <admin@admins-MacBook-Pro.local>
2020-05-25 15:12:32 +0800

    add shell script to process QCD samples parallelly

---
+++ hist-plot/run_QCD.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+/usr/bin/time -o timelog.MC16a.JZ1W root -b -q "makingHist.C(\"MC16a_JZ1W\", \"outTree\")" >& log.MC16a.JZ1W &
+/usr/bin/time -o timelog.MC16a.JZ2W root -b -q "makingHist.C(\"MC16a_JZ2W\", \"outTree\")" >& log.MC16a.JZ2W &
+/usr/bin/time -o timelog.MC16a.JZ3W root -b -q "makingHist.C(\"MC16a_JZ3W\", \"outTree\")" >& log.MC16a.JZ3W &
+/usr/bin/time -o timelog.MC16a.JZ4W root -b -q "makingHist.C(\"MC16a_JZ4W\", \"outTree\")" >& log.MC16a.JZ4W &
+/usr/bin/time -o timelog.MC16a.JZ5W root -b -q "makingHist.C(\"MC16a_JZ5W\", \"outTree\")" >& log.MC16a.JZ5W &
+/usr/bin/time -o timelog.MC16a.JZ6W root -b -q "makingHist.C(\"MC16a_JZ6W\", \"outTree\")" >& log.MC16a.JZ6W &
+/usr/bin/time -o timelog.MC16a.JZ7W root -b -q "makingHist.C(\"MC16a_JZ7W\", \"outTree\")" >& log.MC16a.JZ7W &
+/usr/bin/time -o timelog.MC16a.JZ8W root -b -q "makingHist.C(\"MC16a_JZ8W\", \"outTree\")" >& log.MC16a.JZ8W &
+/usr/bin/time -o timelog.MC16a.JZ9W root -b -q "makingHist.C(\"MC16a_JZ9W\", \"outTree\")" >& log.MC16a.JZ9W &
+/usr/bin/time -o timelog.MC16a.JZ10W root -b -q "makingHist.C(\"MC16a_JZ10W\", \"outTree\")" >& log.MC16a.JZ10W &
+/usr/bin/time -o timelog.MC16a.JZ11W root -b -q "makingHist.C(\"MC16a_JZ11W\", \"outTree\")" >& log.MC16a.JZ11W &
+/usr/bin/time -o timelog.MC16a.JZ12W root -b -q "makingHist.C(\"MC16a_JZ12W\", \"outTree\")" >& log.MC16a.JZ12W &
+
+/usr/bin/time -o timelog.MC16d.JZ1W root -b -q "makingHist.C(\"MC16d_JZ1W\", \"outTree\")" >& log.MC16d.JZ1W &
+/usr/bin/time -o timelog.MC16d.JZ2W root -b -q "makingHist.C(\"MC16d_JZ2W\", \"outTree\")" >& log.MC16d.JZ2W &
+/usr/bin/time -o timelog.MC16d.JZ3W root -b -q "makingHist.C(\"MC16d_JZ3W\", \"outTree\")" >& log.MC16d.JZ3W &
+/usr/bin/time -o timelog.MC16d.JZ4W root -b -q "makingHist.C(\"MC16d_JZ4W\", \"outTree\")" >& log.MC16d.JZ4W &
+/usr/bin/time -o timelog.MC16d.JZ5W root -b -q "makingHist.C(\"MC16d_JZ5W\", \"outTree\")" >& log.MC16d.JZ5W &
+/usr/bin/time -o timelog.MC16d.JZ6W root -b -q "makingHist.C(\"MC16d_JZ6W\", \"outTree\")" >& log.MC16d.JZ6W &
+/usr/bin/time -o timelog.MC16d.JZ7W root -b -q "makingHist.C(\"MC16d_JZ7W\", \"outTree\")" >& log.MC16d.JZ7W &
+/usr/bin/time -o timelog.MC16d.JZ8W root -b -q "makingHist.C(\"MC16d_JZ8W\", \"outTree\")" >& log.MC16d.JZ8W &
+/usr/bin/time -o timelog.MC16d.JZ9W root -b -q "makingHist.C(\"MC16d_JZ9W\", \"outTree\")" >& log.MC16d.JZ9W &
+/usr/bin/time -o timelog.MC16d.JZ10W root -b -q "makingHist.C(\"MC16d_JZ10W\", \"outTree\")" >& log.MC16d.JZ10W &
+/usr/bin/time -o timelog.MC16d.JZ11W root -b -q "makingHist.C(\"MC16d_JZ11W\", \"outTree\")" >& log.MC16d.JZ11W &
+/usr/bin/time -o timelog.MC16d.JZ12W root -b -q "makingHist.C(\"MC16d_JZ12W\", \"outTree\")" >& log.MC16d.JZ12W &
+
+/usr/bin/time -o timelog.MC16e.JZ1W root -b -q "makingHist.C(\"MC16e_JZ1W\", \"outTree\")" >& log.MC16e.JZ1W &
+/usr/bin/time -o timelog.MC16e.JZ2W root -b -q "makingHist.C(\"MC16e_JZ2W\", \"outTree\")" >& log.MC16e.JZ2W &
+/usr/bin/time -o timelog.MC16e.JZ3W root -b -q "makingHist.C(\"MC16e_JZ3W\", \"outTree\")" >& log.MC16e.JZ3W &
+/usr/bin/time -o timelog.MC16e.JZ4W root -b -q "makingHist.C(\"MC16e_JZ4W\", \"outTree\")" >& log.MC16e.JZ4W &
+/usr/bin/time -o timelog.MC16e.JZ5W root -b -q "makingHist.C(\"MC16e_JZ5W\", \"outTree\")" >& log.MC16e.JZ5W &
+/usr/bin/time -o timelog.MC16e.JZ6W root -b -q "makingHist.C(\"MC16e_JZ6W\", \"outTree\")" >& log.MC16e.JZ6W &
+/usr/bin/time -o timelog.MC16e.JZ7W root -b -q "makingHist.C(\"MC16e_JZ7W\", \"outTree\")" >& log.MC16e.JZ7W &
+/usr/bin/time -o timelog.MC16e.JZ8W root -b -q "makingHist.C(\"MC16e_JZ8W\", \"outTree\")" >& log.MC16e.JZ8W &
+/usr/bin/time -o timelog.MC16e.JZ9W root -b -q "makingHist.C(\"MC16e_JZ9W\", \"outTree\")" >& log.MC16e.JZ9W &
+/usr/bin/time -o timelog.MC16e.JZ10W root -b -q "makingHist.C(\"MC16e_JZ10W\", \"outTree\")" >& log.MC16e.JZ10W &
+/usr/bin/time -o timelog.MC16e.JZ11W root -b -q "makingHist.C(\"MC16e_JZ11W\", \"outTree\")" >& log.MC16e.JZ11W &
+/usr/bin/time -o timelog.MC16e.JZ12W root -b -q "makingHist.C(\"MC16e_JZ12W\", \"outTree\")" >& log.MC16e.JZ12W &
