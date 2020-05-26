da9bc5dd0859a2ad49096b4534c3b3bd7d951d18
Changxu Wei <changxu@lxplus767.cern.ch>
2020-05-26 09:27:54 +0200

    debug for it

--- hist-plot/listAndPlot.sh
+++ hist-plot/listAndPlot.sh
@@ -3,9 +3,10 @@
 hadd -f $1.root $1_*.root
 
 rm outfile
-for i in $(find *$1*); do
+for i in $(find $1_*.root); do
     echo $i >> outfile
 done
 
-root -b -q "trial.C(\"$1\")"
+root -b -q "draw.C(\"$1.root\")"
+root -b -q "drawpt.C(\"outfile\", \"$1.root\")"
 
