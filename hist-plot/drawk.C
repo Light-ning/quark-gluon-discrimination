5fb9c53ab7b9af594c858b1a4c193b940ff1a5c6
zhouyan17 <1945220333@qq.com>
2020-07-03 16:19:35 +0800

    修改画图格式以匹配文档

--- hist-plot/drawk.C
+++ hist-plot/drawk.C
@@ -15,7 +15,7 @@
 
 using namespace std;
 
-TCanvas *c = new TCanvas("c","c",1100,1100);
+TCanvas *c = new TCanvas("c","c",1600,1100);
 TPad *pad1 = new TPad("pad1","pad1",0,0.31,1,1);
 TPad *pad2 = new TPad("pad2","pad2",0,0,1,0.30);
 
@@ -23,7 +23,7 @@ TFile *f1;
 TFile *f2;
 float lum = 0;
 
-void draw(TString name);
+void draw(TString name, float xmin, float xmax, float ymin, float ymax);
 
 int main(int argc,char **argv) {
     SetAtlasStyle ();
@@ -39,13 +39,13 @@ int main(int argc,char **argv) {
     
     TString dataset = argv[1];
     if(dataset=="a" || dataset=="15+16"){
-        f1 = new TFile("outMC/mjje.root");
-        f2 = new TFile("outdata/mjj18.root");
+        f1 = new TFile("outMC/mjja.root");
+        f2 = new TFile("outdata/mjj15+16.root");
         lum = 36.17;
     }
     if(dataset=="d" || dataset=="17"){
-        f1 = new TFile("outMC/mjje.root");
-        f2 = new TFile("outdata/mjj18.root");
+        f1 = new TFile("outMC/mjjd.root");
+        f2 = new TFile("outdata/mjj17.root");
         lum = 44.31;
     }
     if(dataset=="e" || dataset=="18"){
@@ -54,67 +54,67 @@ int main(int argc,char **argv) {
         lum = 58.45;
     }
     
-    draw("HistMjj");
+    draw("HistMjj", 1000, 8500, 0.05, 500000000);
     c->SaveAs("mjj.png");
     
-    draw("HistLeadingJetPt");
+    draw("HistLeadingJetPt", 0, 3000, 0.5, 1000000000);
     c->SaveAs("lpt.png");
     
-    draw("HistSubJetPt");
+    draw("HistSubJetPt", 0, 3000, 0.5, 1000000000);
     c->SaveAs("spt.png");
     
-    draw("HistLjeteta");
+    draw("HistLjeteta", -3.1, 3.1, 0.5, 200000000);
     c->SaveAs("leta.png");
     
-    draw("HistSjeteta");
+    draw("HistSjeteta", -3.1, 3.1, 0.5, 200000000);
     c->SaveAs("seta.png");
     
-    draw("HistLjetphi");
+    draw("HistLjetphi", -3.1, 3.1, 0.5, 50000000);
     c->SaveAs("lphi.png");
     
-    draw("HistSjetphi");
+    draw("HistSjetphi", -3.1, 3.1, 0.5, 50000000);
     c->SaveAs("sphi.png");
     
-    draw("HistyStar");
+    draw("HistyStar", -1.2, 1.2, 0.5, 200000000);
     c->SaveAs("ystar.png");
     
-    draw("Histdeltaeta");
+    draw("Histdeltaeta", -3.1, 3.1, 0.5, 200000000);
     c->SaveAs("deltaeta.png");
     
-    draw("Histdeltaphi");
+    draw("Histdeltaphi", 0, 3.1, 0.5, 1000000000);
     c->SaveAs("deltaphi.png");
 }
 
-void draw(TString name){
+void draw(TString name, float xmin, float xmax, float ymin, float ymax){
     TH1D *mc = (TH1D*) f1->Get(name);
     TH1D *data = (TH1D*) f2->Get(name);
     
     pad1->cd();
-    data->SetLineColor(7);
-    data->SetMarkerColor(7);
-    data->Draw();
     mc->Scale(lum);
-    mc->SetLineColor(2);
-    mc->SetMarkerColor(2);
-    mc->Draw("same");
-    myText(       0.6,  0.85, 1, "#sqrt{s}= 13 TeV");
-    myMarkerText( 0.8, 0.7, 2, 20, "MC",1);
-    myMarkerText( 0.8, 0.6, 7, 20, "data",1);
-    ATLASLabel(0.2,0.15,"Preliminary");
+    mc->SetAxisRange(xmin, xmax, "X");
+    mc->SetAxisRange(ymin, ymax, "Y");
+    mc->SetLineColor(1);
+    mc->SetLineWidth(2);
+    mc->SetMarkerColor(kYellow);
+    mc->SetFillColor(kYellow);
+    mc->Draw("][ hist");
+    data->SetLineColor(1);
+    data->SetMarkerColor(1);
+    data->Draw("same");
+    myText(   0.17,  0.75, 1, "#sqrt{s}= 13 TeV");
+    myBoxText( 0.6, 0.8, 0.05, kYellow, "MC");
+    myMarkerText( 0.6, 0.75, 1, 20, "data",1);
+    ATLASLabel(0.17,0.8,"Preliminary");
     
     TH1D *mcf = (TH1D*)mc->Clone();
     TH1D *dataf = (TH1D*)data->Clone();
     pad2->cd();
     dataf->Divide(mcf);
-    dataf->SetLineColor(7);
-    dataf->SetMarkerColor(7);
-    mcf->Divide(mcf);
-    mcf->SetAxisRange(0, 2, "Y");
-    //mcfdeta->SetAxisRange(0, 2*edatadeta/emcdeta-1, "Y");
-    mcf->SetLineColor(2);
-    mcf->SetMarkerColor(2);
-    mcf->GetYaxis()->SetTitle("data/mc");
+    dataf->SetLineColor(1);
+    dataf->SetMarkerColor(1);
+    dataf->SetAxisRange(xmin, xmax, "X");
+    dataf->SetAxisRange(0.5, 1.5, "Y");
+    dataf->GetYaxis()->SetTitle("data/mc");
     //mcfdeta->SetTitleSize(42,"Y");
-    mcf->Draw();
-    dataf->Draw("same");
+    dataf->Draw();
 }
